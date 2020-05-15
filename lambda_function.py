d8141f47e11860ea3edd4f072df3b446a8c6852d
soumoks <soumoks@gmail.com>
2020-05-14 22:33:18 -0600

    initial commit

---
+++ lambda_function.py
@@ -0,0 +1,7 @@
+"""
+Creates an ECR repository ,if it doesn’t already exist, to store the container images once built
+Uploads the train.py and Dockerfile to an S3 bucket
+Creates a Codebuild project and uses the above files with a buildspec.yml to start the process to build a container push the image to ECR.
+"""
+def lambda_handler(event,context):
+    print(event)
