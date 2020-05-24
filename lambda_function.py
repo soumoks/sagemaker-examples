b4ca7d2560960aa5b955e96ca1f1be0e199e0045
soumoks <soumoks@gmail.com>
2020-05-23 20:14:09 -0600

    changed lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,8 @@ Uploads the train.py and Dockerfile to an S3 bucket
 Creates a Codebuild project and uses the above files with a buildspec.yml to start the process to build a container push the image to ECR.
 """
 def lambda_handler(event,context):
+	"""
+	triggering webhook by changing this file
+	"""
     print("Body content: ")
     print(event)
